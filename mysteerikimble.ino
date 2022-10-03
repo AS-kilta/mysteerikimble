61e54634f9ac16188807dad47aab86fa1c999f64
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2022-10-03 20:39:52 +0300

    Add pinout comments

--- mysteerikimble.ino
+++ mysteerikimble.ino
@@ -6,6 +6,19 @@ void setup() {
 
 int meas;
 
+/* inputs
+ *
+ * popomatic
+ * 3x ADC (hall sensor)
+ */
+
+/* outputs
+ *
+ * 16x goal led
+ * 8x 7-segment led
+ * 8x 7-segment transistor
+ * 1x neopixel (RGB!!)
+ */
 void loop() {
   // put your main code here, to run repeatedly:
   meas = analogRead(A0);
