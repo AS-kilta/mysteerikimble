1f050323ec07d5254fd4bc3723aa6fe25978a8aa
Sampo Saloriutta <sampo.saloriutta@gmail.com>
2022-10-07 20:20:32 +0300

    pin changes for MEGA and bugfixes

--- mysteerikimble.ino
+++ mysteerikimble.ino
@@ -1,29 +1,32 @@
 //int transistorPins[] = {10, 11};
 int transistorPinCount = 6;
-//int segmentPinStart = 2;
-//int segmentPinEnd = 9;
+int transistorPinStart = 2;
+int transistorPinEnd = 7;
 int transistorOffset = 2;
-int sensorPins[] = {};
+int sensorPins[] = {A0, A1, A2};
 int naksPin = 18; //must be 2, 3, 18, 19, 20, 21
-int redPins[] = {};
-int bluePins[] = {};
-int greenPins[] = {};
-int yellowPins[] = {};
+int redPins[] = {31};
+int bluePins[] = {33};
+int greenPins[] = {35};
+int yellowPins[] = {37};
 
 int targetNum = 43;
-unsigned long countDownCentiSec = 15L*60L*100L;
+unsigned long countDownCentiSec = 1000L*10L;//15L*60L*1000L;
 unsigned long timercnt = 0;
 int naksCount = 0;
-unsigned int lastNaksTimer = 0;
+unsigned long lastNaksTimer = 0;
 bool timerStart = false;
 bool blinkState = 0;
+unsigned long lastTime = countDownCentiSec;
 
-char digitStates[10] = {
-  B01110111, B01000001, B00111011, B01101011, B01001101, B01101110, B01111110, B01000011, B01111111, B01001111};
+char digitStates[11] = {
+  B11101101, B00101000, B11001011, B01101011, B00101110, B01100111, B11100111, B00101001, B11101111, B01101111, B00000010};
 
 
 void setupTransistorPins() {
-  TRANSISTORPINS = 0xFF;
+  for(int i = transistorPinStart; i < transistorPinEnd; i++){
+    pinMode(i, OUTPUT);
+  }
 }
 
 void setupSegmentPins() {
@@ -61,10 +64,10 @@ void setupNaksCounter(){
 }
 
 void onNaks(){
-  if(lastNaksTimer = 100) {
+  if(lastNaksTimer > 100) {
     naksCount++;
     lastNaksTimer = 0;
-    blinkStart = 1;
+    blinkState = 1;
   }
 }
 
@@ -113,13 +116,14 @@ void blinkLeds(){
     
   }  
   unsigned long currTime = countDownCentiSec;
-  if((lasTime - currTime) > 50) {
-    blinkState++;    
-  }
+  if((lastTime - currTime) > 50) {
+    blinkState++;
+    lastTime = currTime;    
+  }  
 }
 
 void setupLedPins(){
-  for(int i = 0; i < 4; i++){
+  for(int i = 0; i < 1; i++){
     pinMode(redPins[i], OUTPUT);
     pinMode(bluePins[i], OUTPUT);
     pinMode(greenPins[i], OUTPUT);
@@ -127,32 +131,52 @@ void setupLedPins(){
   }
 }
 
-int debugIndexOffset = 2;
+int debugIndexOffset = 0;
 
 ISR(TIMER0_COMPA_vect){//timer0 interrupt 50Hz
 
   if(timerStart){
-    countDownCentiSec -= 2;
- 
- 
-    int displayIndex = timercnt % 2;
-    int digitIndex = timercnt % transistorPinCount; 
-
-    if((digitIndex + debugIndexOffset) < 2) {
-      targetNum = (int)(countDownCentiSec  % 100L); //1/100 seconds
-    }else if((digitIndex + debugIndexOffset ) < 4){
-      targetNum = (int)((countDownCentiSec / 100L) % 60L); //seconds
-    }else if((digitIndex + debugIndexOffset) < 6){
-      targetNum = (int)(countDownCentiSec / 60L / 100L); //minutes
+    if(naksCount < 150) {
+      countDownCentiSec -= 4;
+    }else{
+      countDownCentiSec = 0;
     }
 
+ 
+ 
+    int displayIndex = !(timercnt % 2);
+    int digitIndex = timercnt % transistorPinCount; 
+    char dp = B00010000;
+
+    if((digitIndex + debugIndexOffset) < 2) {
+      targetNum = (int)(countDownCentiSec / 60L / 1000L); //minutes
+    }else if((digitIndex + debugIndexOffset ) < 4){
+      targetNum = (int)((countDownCentiSec / 1000L) % 60L); //seconds
+      //targetNum = (int)(countDownCentiSec / 60L / 100L); //minutes
+    }else if((digitIndex + debugIndexOffset) < 6){
+      targetNum = (int)(countDownCentiSec  % 1000L); //1/100 seconds
+    }
+    
     int divider = displayIndex ? 10 : 1;
     int digit = (targetNum / divider) % 10;
     // Serial.println(digit);
     //char transistorMask = 0x01 << (transistorOffset + digitIndex);
 
-    PORTK = digitStates[digit];
-    TRANSISTORPORT = 0x01 << (transistorOffset + digitIndex);
+    if(naksCount >= 150) {
+      digit = 10;
+    }
+
+    if(digitIndex == 1 || digitIndex == 3){
+      PORTK = dp | digitStates[digit];
+    }else{
+       PORTK = digitStates[digit];   
+    }
+    
+    for(int i = transistorPinStart; i < transistorPinEnd; i++){
+      digitalWrite(i, LOW);      
+    }
+    digitalWrite(digitIndex + transistorOffset, HIGH);
+
     //PORTD = digitStates[digit] << 2; 
     //PORTB = transistorMask | (0xC0 & digitStates[digit]) >> 6;
 
@@ -184,12 +208,10 @@ bool measure(){
   meas0 = analogRead(sensorPins[0]);
   meas1 = analogRead(sensorPins[1]);
   meas2 = analogRead(sensorPins[2]);
-    
-  return (meas0 > 800) && (meas1 > 800) && (meas2 > 800);  
+  return (meas0 > 600);// && (meas1 > 800) && (meas2 > 800);  
     
 }
 
-unsigned long lastTime = countDownCentiSec;
 
 /* inputs
  *
@@ -208,11 +230,17 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(!timerStart) {
     timerStart = measure();
-  }else if(naksCount > 50) {
-      timerStart = false;
+    if(timerStart){
+      digitalWrite(31, HIGH);
+      digitalWrite(33, HIGH);
+      digitalWrite(35, HIGH);
+      digitalWrite(37, HIGH);
+    }    
   }
 
-  if(blinkState){
-    blinkLeds();
-  }
+  Serial.println(naksCount);
+
+  //if(blinkState){
+  //  blinkLeds();
+  //}
 }
